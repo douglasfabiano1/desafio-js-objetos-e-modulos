9acda1d29ed62d785abf8230b5819181fec95469
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-01-05 17:39:16 -0300

    condition changed

--- js/services/shipping-service.js
+++ js/services/shipping-service.js
@@ -4,7 +4,7 @@ export function shipment(order){
 
   if(basicValue < 100.0){
       shipmentValue = 20.0;
-  }else if(basicValue < 200.0){
+  }else if(basicValue >= 100.0 && basicValue < 200.0){
       shipmentValue = 12.0;
   }else {
       shipmentValue = 0.0;
