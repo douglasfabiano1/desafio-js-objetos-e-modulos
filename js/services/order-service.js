4e8a15fdbbb9429a1fcee2cfe7ee894f5cb1ed4a
Douglas Fabiano <douglasfabiano23@gmail.com>
2023-01-05 17:28:28 -0300

    Input data, order-service correction, main created

--- js/services/order-service.js
+++ js/services/order-service.js
@@ -2,7 +2,7 @@ import * as shippingService from './shipping-service.js';
 
 export function total(order){
 
-  const shipment = shippingService.shipment(order.basic);
+  const shipment = shippingService.shipment(order);
   const discount = order.basic * order.discount / 100;
 
   return order.basic - discount + shipment;
